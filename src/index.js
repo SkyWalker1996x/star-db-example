174b7bebf80db554fe45b37fcd95444d12b496fc
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-07-08 11:54:57 +0300

    create basic structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
 
 ReactDOM.render(
   <React.StrictMode>
-      <h1>StarDB</h1>
+    <App/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
