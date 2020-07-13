4cb4bff34854063a37240657f7e374aa65a841c2
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-07-13 13:16:56 +0300

    upd App, ErrorButton, PersonDetails component, add PeoplePage component: transposition logic block from App to PeoplePage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,11 +6,22 @@ import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 
 import "./app.css";
+import ErrorButton from "../error-button";
+import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
 
 class App extends Component {
   state = {
     selectedItem: 7,
+    hasError: false,
   };
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+    this.setState({
+      hasError: true,
+    });
+  }
 
   onItemSelected = (id) => {
     this.setState({
@@ -19,19 +30,28 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
     return (
       <div className="container">
         <Header />
         <RandomPlanet />
+        <ErrorButton />
 
-        <div className="row mb2">
+        {/*<div className="row mb2">
           <div className="col-md-6">
             <ItemList onItemSelected={this.onItemSelected} />
           </div>
           <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedItem}/>
+            <PersonDetails personId={this.state.selectedItem} />
           </div>
-        </div>
+        </div>*/}
+
+        <PeoplePage/>
+        <PeoplePage/>
+        <PeoplePage/>
       </div>
     );
   }
--- src/components/error-button/error-button.js
+++ src/components/error-button/error-button.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import './error-button.css';
-
 export default class ErrorButton extends Component {
 
     state = {
