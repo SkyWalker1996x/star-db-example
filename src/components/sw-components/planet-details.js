e4a062ff2d2ed32d35c3aa75ab837f7443ff2acd
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-07-23 12:08:25 +0300

    refactor: implement transformation data function and StarshipDetails & PlanetDetails

--- src/components/sw-components/planet-details.js
+++ src/components/sw-components/planet-details.js
@@ -2,8 +2,7 @@ import React from "react";
 import ItemDetails, { Record } from "../item-details";
 import { withSwapiService } from "../hoc";
 
-const PlanetDetails = ({ id, swapiService }) => {
-  const { getPlanet, getPlanetImage } = swapiService;
+const PlanetDetails = ({ id, getPlanet, getPlanetImage }) => {
   return (
     <ItemDetails itemId={id} getData={getPlanet} getImageUrl={getPlanetImage}>
       <Record field={"name"} label={"Name"} />
@@ -12,4 +11,11 @@ const PlanetDetails = ({ id, swapiService }) => {
   );
 };
 
-export default withSwapiService(PlanetDetails);
+const mapMethodToProps = (swapiService) => {
+  return {
+    getData: swapiService.getPlanet,
+    getImageUrl: swapiService.getPlanetImage,
+  };
+};
+
+export default withSwapiService(PlanetDetails, mapMethodToProps);
--- src/components/sw-components/starship-details.js
+++ src/components/sw-components/starship-details.js
@@ -2,18 +2,20 @@ import React from "react";
 import ItemDetails, { Record } from "../item-details";
 import { withSwapiService } from "../hoc";
 
-const StarshipDetails = ({ id, swapiService }) => {
-  const { getStarship, getStarshipImage } = swapiService;
+const StarshipDetails = ({ id, getData, getImageUrl }) => {
   return (
-    <ItemDetails
-      itemId={id}
-      getData={getStarship}
-      getImageUrl={getStarshipImage}
-    >
+    <ItemDetails itemId={id} getData={getData} getImageUrl={getImageUrl}>
       <Record field={"name"} label={"Name"} />
       <Record field={"length"} label={"length"} />
     </ItemDetails>
   );
 };
 
-export default withSwapiService(StarshipDetails);
+const mapMethodToProps = (swapiService) => {
+  return {
+    getData: swapiService.getStarship,
+    getImageUrl: swapiService.getStarshipImage,
+  };
+};
+
+export default withSwapiService(StarshipDetails, mapMethodToProps);
