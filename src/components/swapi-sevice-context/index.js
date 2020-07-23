fb1818958e4b9c37efdc03a1fcf0c206a72d49bf
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-07-23 09:05:19 +0300

    refactor: change structure of directory SW-context

--- src/components/swapi-sevice-context/index.js
+++ src/components/swapi-sevice-context/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const {
-  Provider: SwapiServiceProvider,
-  Consumer: SwapiServiceConsumer,
+    Provider: SwapiServiceProvider,
+    Consumer: SwapiServiceConsumer,
 } = React.createContext();
 
 export { SwapiServiceProvider, SwapiServiceConsumer };
